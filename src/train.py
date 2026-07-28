1af2373791e650c0a0c152fa1002635ed7b1e726
hadifar <hadifar.amir@gmail.com>
2026-07-28 22:39:22 +0200

    Revert "fix catboost predict returning column vector"
    This reverts commit 6a4eacbe06d7e860ea8430dae2a7e44c23040e59.

--- src/train.py
+++ src/train.py
@@ -51,8 +51,7 @@ def main() -> None:
         model = build_model()
         weights = compute_sample_weight("balanced", y.iloc[train_idx])
         model.fit(x.iloc[train_idx], y.iloc[train_idx], sample_weight=weights)
-        # CatBoost returns an (n, 1) column vector rather than sklearn's 1-D array.
-        predictions = model.predict(x.iloc[val_idx]).ravel()
+        predictions = model.predict(x.iloc[val_idx])
         oof.iloc[val_idx] = predictions
 
         fold_score = balanced_accuracy_score(y.iloc[val_idx], predictions)
